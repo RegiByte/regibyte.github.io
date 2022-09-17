0a9e4780621b7a01ee8b4c7bca937cb470cc83f7
Reginaldo Junior <reginaldo.junior696@gmail.com>
2022-09-17 10:08:48 -0300

    added two inline tools

---
+++ src/components/tools/generateUuid.svelte
@@ -0,0 +1,17 @@
+<script>
+  import {v4 as uuid} from 'uuid'
+  let current = ''
+
+  function createUuid() {
+    current = uuid().toString()
+  }
+</script>
+
+<div class='text-base text-zinc-700 dark:text-zinc-400'>
+  <button on:click={createUuid} class='border rounded-lg px-2 py-1'>Generate a timestamped UUID here</button>
+
+  {#if current}
+    <span class='px-2'>{current}</span>
+  {/if}
+
+</div>
---
+++ src/components/tools/getLocation.svelte
@@ -0,0 +1,20 @@
+<script>
+  let currentLocation = ''
+
+  function getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      currentLocation = `Lat ${coords.latitude}, Long ${coords.longitude}`
+    }, err => {
+      alert('was not possible to determine current location')
+    })
+  }
+</script>
+
+<div class='text-base text-zinc-700 dark:text-zinc-400'>
+  <button on:click={getCurrentLocation} class='border rounded-lg px-2 py-1'>Get Current GeoLocation here</button>
+
+  {#if currentLocation}
+    <span class='px-2'>{currentLocation}</span>
+  {/if}
+
+</div>
