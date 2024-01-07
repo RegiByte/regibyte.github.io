4036868025e9d491be54bf2670f3895f95c2b1ea
Reginaldo Junior <reginaldo.junior@ae.studio>
2024-01-07 13:59:17 -0300

    fix bug with parsing of url parameters on floating text effect

--- src/components/effects/text-flow-field/helpers.ts
+++ src/components/effects/text-flow-field/helpers.ts
@@ -1,16 +1,21 @@
 export const getInitialText = (initial = 'Hello World', queryKey = 'text') => {
-  if (typeof window === "undefined") return initial;
+  if (typeof window === 'undefined') return initial;
   let query = new URLSearchParams(window.location.search);
-  return query.get(queryKey) || initial;
-}
+  let text = query.get(queryKey);
+  if (!text) return initial;
+  return decodeURIComponent(text);
+};
 
-export const getInitialColors = (initial = ['red', 'blue', 'fuchsia'], queryKey = 'colors') => {
-  if (typeof window === "undefined") return initial;
+export const getInitialColors = (
+  initial = ['red', 'blue', 'fuchsia'],
+  queryKey = 'colors',
+) => {
+  if (typeof window === 'undefined') return initial;
   let query = new URLSearchParams(window.location.search);
   let colors = query.get(queryKey);
   if (!colors) return initial;
-  return colors.split(',');
-}
+  return decodeURIComponent(colors).split(',');
+};
 
 export const getStopPoints = (colors: string[]) => {
   const increment = 100 / (colors.length - 1);
@@ -19,4 +24,4 @@ export const getStopPoints = (colors: string[]) => {
     stopPoints.push(`${i * increment}%`);
   }
   return stopPoints;
-}
+};
